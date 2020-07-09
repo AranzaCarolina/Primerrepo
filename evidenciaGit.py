116b3fd74af30d2550a54be244bb3b96bd55527e
Aranza Carolina Mejorado Casas <mejoradocarolina@gmail.com>
2020-07-09 10:15:05 -0500

    evidenciaGit

---
+++ evidenciaGit.py
@@ -0,0 +1,13 @@
+#Escribir un programa que pida al usuario 2 numeros enteros
+#y calcular la suma desde el numero 1 hasta el numero 2
+#imprimir el resultado de la suma
+
+print("bienvenido")
+num1 = int(input("dime el primer numero: "))
+num2 = int(input("Dime el segundo numero: "))
+
+suma = num1+num2
+for suma in range(num1,num2):
+    suma+=num
+print(f'la suma es {suma}')
+

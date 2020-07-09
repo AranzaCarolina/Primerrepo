acd6b7a6e83161f819b381dc60168fe397da309b
Aranza Carolina Mejorado Casas <mejoradocarolina@gmail.com>
2020-07-09 10:56:15 -0500

    evidenciaWhile

---
+++ evidenciaWhile.py
@@ -0,0 +1,11 @@
+#Programa que muestre al usuario al sig menu
+
+print("Bienvenido, oprima la opcion que desea")
+print("operaciones: [1]suma,[2]resta,[3]multitplicacion,[4]division.[5]salir")
+eleccion = int(input("Selecciona una de las opciones anteriores: "))
+if eleccion == 1:
+    #suma
+    n1 = int(input("introduce el primer numero: "))
+    n2 = int(input("introduce el segundo numero: "))
+    print(f"{n1} + {n2} = {n1 + n2}")
+    espacio = input()

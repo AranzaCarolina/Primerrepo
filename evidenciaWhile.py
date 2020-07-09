39559595563d9e7d975f6a9ec7c4a4c10276745f
Aranza Carolina Mejorado Casas <mejoradocarolina@gmail.com>
2020-07-09 14:22:05 -0500

    evidenciaWhile

--- evidenciaWhile.py
+++ evidenciaWhile.py
@@ -1,23 +1,24 @@
-#Programa que muestre al usuario al sig menu
+#Menu Ciclico
 
-print("Bienvenido, oprima la opcion que desea")
-print("operaciones: [1]suma,[2]resta,[3]multitplicacion,[4]division.[5]salir")
-eleccion = int(input("Selecciona una de las opciones anteriores: "))
-if eleccion == 1:
-    #suma
-    n1 = int(input("introduce el primer numero: "))
-    n2 = int(input("introduce el segundo numero: "))
-    print(f"{n1} + {n2} = {n1 + n2}")
-    espacio = input()
-elif eleccion == 2:
-    #resta
-    n1 = int(input("Introduce el primer numero: "))
-    n2 = int(input("Introduce el segundo numero: "))
-    print(f"{n1} - {n2} = {n1 - n2}")
-    espacio = input()
-elif eleccion == 3:
-    #multiplicacion
-    n1 = int(input("Introduce el primer numero: "))
-    n2 = int(input("Introduce el segundo numero: "))
-    print(f"{n1} * {n2} = {n1 * n2}")
-    espacio = input()
+while True:
+    print("operaciones: [1]suma, [2]resta, [3]multitplicacion, [4]division, [5]salir")
+    eleccion = input("Selecciona una de las opciones anteriores: ")
+
+    if eleccion == "1" or eleccion == "2" or eleccion == "3" or eleccion == "4":
+        n1 = int(input("introduce el primer numero: "))
+        n2 = int(input("introduce el segundo numero: "))
+    if eleccion == "1":
+        #suma
+        print(f"{n1} + {n2} = {n1 + n2}")
+    elif eleccion == "2":
+     #resta
+     print(f"{n1} - {n2} = {n1 - n2}")
+    elif eleccion == "3":
+     #multiplicacion
+     print(f"{n1} * {n2} = {n1 * n2}")
+    elif eleccion == "4":
+     #division
+     print(f"{n1} / {n2} = {n1 / n2}")
+    elif eleccion == "5":
+        print ("Adios")
+        break
